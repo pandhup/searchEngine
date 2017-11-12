73064c9e47073aeed80e4081771958f150c9b15d
Dandy Ramadhan <dandyarir@gmail.com>
2017-11-13 04:52:42 +0700

    edit distance jadi euclidian dan presentasi

---
+++ README.md
@@ -0,0 +1,15 @@
+# Search dokumen dengan Flask
+implementasi search engine dengan framework Flask menggunakan  **Python 2.7**
+
+### Installasi requirement
+```
+pip install -r requirements.txt
+```
+
+### Running
+```
+python run.py
+```
+
+### Acknowledgments
+* https://github.com/rianrajagede/flask-search.git
---
+++ stki_scripts/w5.py
@@ -0,0 +1,27 @@
+"""
+author rochanaph
+September 21 2017
+"""
+import math
+def euclidean(vector1, vector2):
+    """
+    fungsi untuk menghitung jarak antara 2 vektor dengan rumus euclidean ddistance
+    :param vector1: vektor 1
+    :param vector2: vektor
+    :return:
+    """
+    dist = [(a - b)**2 for a, b in zip(vector1, vector2)]
+    dist = math.sqrt(sum(dist))
+    return dist
+
+def cosine(vector1, vector2):
+    """
+    :param vector1:
+    :param vector2:
+    :return:
+    """
+
+    dot  = sum([a*b for a,b in zip(vector1, vector2)])
+    mag1 = math.sqrt(sum([a**2 for a in vector1]))
+    mag2 = math.sqrt(sum([a**2 for a in vector2]))
+    return round(dot/(mag1*mag2),3)*100
